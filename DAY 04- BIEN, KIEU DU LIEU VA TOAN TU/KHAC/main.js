86bcd290e540a51ff1de65ee2025e30ab2c4b614
lequangthong1996 <lequangthong1996@gmail.com>
2023-07-08 13:49:25 +0700

    Signed-off-by: lequangthong1996 <lequangthong1996@gmail.com>

---
+++ DAY 04- BIEN, KIEU DU LIEU VA TOAN TU/KHAC/main.js
@@ -0,0 +1,24 @@
+let intro = 'Hello World'
+        document.getElementById('intro').innerHTML = intro
+
+    document.write (intro)
+    document.write ('<br>')
+    document.write (intro)
+    document.write ('<br>')
+    document.write (intro)
+    document.write ('<br>')
+    document.write (intro)
+    document.write ('<br>')
+    document.write (intro)
+    document.write ('<br>')
+    document.write (intro)
+    document.write ('<br>')
+    document.write (intro)
+    document.write ('<br>')
+    document.write (intro)
+    document.write ('<br>')
+    document.write (intro)
+    document.write ('<br>')
+    document.write (intro)
+    document.write ('<b')
+    
